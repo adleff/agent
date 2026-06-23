bce12b66625f6128b12d6c474a2b7db91dfa51be
Adam Leff <adamleff@gmail.com>
2026-06-22 23:12:38 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY not set")
+
+client = genai.Client(api_key=api_key)
+
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+response = client.models.generate_content(
+    model="gemini-1.5-flash",
+    contents=contents,
+)
+print(response)
