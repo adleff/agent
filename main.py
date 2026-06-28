ed2bd5f79cb69e25979e5abb9339280a4179d9cb
adleff <adamleff@gmail.com>
2026-06-28 03:43:57 -0700

    basic API checks

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+
+# from google.genai.types import UsageMetadataOrDict
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+contents = args.user_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,10 +18,14 @@ if not api_key:
 
 client = genai.Client(api_key=api_key)
 
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=contents,
 )
+
+
+if response.usage_metadata is None:
+    raise RuntimeError("no usage metadata returned")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
