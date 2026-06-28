ad9ba697bb4bdeab46d93a269481c6ae7956856a
adleff <adamleff@gmail.com>
2026-06-28 04:54:16 -0700

    added --verbose flag and calculator app

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
-
-# from google.genai.types import UsageMetadataOrDict
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
-contents = args.user_prompt
+# contents = args.user_prompt
+
+messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,12 +24,15 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=contents,
+    contents=messages,
 )
-
 
 if response.usage_metadata is None:
     raise RuntimeError("no usage metadata returned")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
