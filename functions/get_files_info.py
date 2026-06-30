5bb470ebb27b07beb830ddfa110abd6da63dc4c8
adleff <adamleff@gmail.com>
2026-06-30 02:31:45 -0700

    added get_file_content.py and associated test module

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,21 +8,23 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
         if not os.path.commonpath([target_dir, working_dir_abs]) == working_dir_abs:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return (
+                f'Error: Cannot list "{directory}"'
+                f"as it is outside the permitted working directory"
+            )
 
         if not os.path.isdir(f"{target_dir}"):
             return f'Error: "{directory}" is not a directory'
 
-        files = os.listdir(target_dir)
-        file_contents = []
-        for file in files:
+        files_info = []
+        for file in os.listdir(target_dir):
             full_path = os.path.join(target_dir, file)
-            file_contents.append(
+            files_info.append(
                 f"- {file}: "
                 f"file_size={os.path.getsize(full_path)} bytes, "
                 f"is_dir={os.path.isdir(full_path)}"
             )
-        return "\n".join(file_contents)
+        return "\n".join(files_info)
 
     except Exception as e:
         return f"Error: {str(e)}"
