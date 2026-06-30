4e39db4d283ef7ba74c5b67d67a969f5da3b4501
adleff <adamleff@gmail.com>
2026-06-30 00:18:47 -0700

    ch2lesson3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,18 +4,25 @@ import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
 
     try:
-        if not os.path.isdir(directory):
-            return f'Error: "{directory}" is not a directory'
-
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-        valid_target_dir = (
-            os.path.commonpath([target_dir, working_dir_abs]) == working_dir_abs
-        )
-        if not valid_target_dir:
-            raise ValueError(
-                f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.commonpath([target_dir, working_dir_abs]) == working_dir_abs:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(f"{target_dir}"):
+            return f'Error: "{directory}" is not a directory'
+
+        files = os.listdir(target_dir)
+        file_contents = []
+        for file in files:
+            full_path = os.path.join(target_dir, file)
+            file_contents.append(
+                f"- {file}: "
+                f"file_size={os.path.getsize(full_path)} bytes, "
+                f"is_dir={os.path.isdir(full_path)}"
             )
-        return f'Success: "{directory}" is within the working directory'
+        return "\n".join(file_contents)
+
     except Exception as e:
         return f"Error: {str(e)}"
