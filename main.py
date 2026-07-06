542d61c0078275b8e47f2f401b6eef193c97fa2d
adleff <adamleff@gmail.com>
2026-07-05 17:04:10 -0700

    finished openrouter conversion

--- main.py
+++ main.py
@@ -2,52 +2,37 @@ import argparse
 import os
 
 from dotenv import load_dotenv
-
-# from google import genai
-# from google.genai import types
 from openai import OpenAI
-
-# parser = argparse.ArgumentParser(description="Chatbot")
-# parser.add_argument("user_prompt", type=str, help="User prompt")
-# parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-# args = parser.parse_args()
-# contents = args.user_prompt
-
-# messages: list[types.Content] = [
-#    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-# ]
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 if not api_key:
     raise RuntimeError("OPENROUTER_API_KEY not set")
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [{"role": "user", "content": args.user_prompt}]
+
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
-# client = genai.Client(api_key=api_key)
+
 response = client.chat.completions.create(
     model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        },
-    ],
+    messages=messages,
 )
 
-# response = client.models.generate_content(
-#    model="gemini-2.5-flash",
-#    contents=messages,
-# )
+if not response.usage:
+    raise RuntimeError("no usage metadata returned")
 
-# if response.usage_metadata is None:
-#    raise RuntimeError("no usage metadata returned")
-#
-# if args.verbose:
-#    print(f"User prompt: {args.user_prompt}")
-#    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-#    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-print(response.choices[0].message.content)
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response: {response.choices[0].message.content}")
+else:
+    print(response.choices[0].message.content)
